66b31b195a318c5611ce3a204bd4ed06b2dc1a69
Akshay Kumar Chaurasia <ac.akshay18@gmail.com>
2022-04-29 19:46:44 +0530

    registration and login

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import *
 from django.conf.urls import url
 
@@ -6,4 +7,6 @@ urlpatterns = [
     path('abcd/<str:id>', AbcdView.as_view(), name="abcd name"),
     path('country/<str:country>/type/<str:type>/variant/<str:variant>/start/<str:start>/end/<str:end>', DetailView.as_view()),
     path('map/<str:start>',MapView.as_view()),
+    path('register', views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
 ]
