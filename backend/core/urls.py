6d7f6eb01a03fe22279a2907fb4e214261950709
hardiksiloiya <hardiksiloiya@gmail.com>
2022-04-30 00:59:52 +0530

    Backend

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('abcd/<str:id>', AbcdView.as_view(), name="abcd name"),
     path('country/<str:country>/type/<str:type>/variant/<str:variant>/start/<str:start>/end/<str:end>', DetailView.as_view()),
     path('map/<str:start>',MapView.as_view()),
+    path('summary/<str:country>',CountrySumView.as_view()),
+    path('world/variant_mix',PieChar.as_view()),
 ]
